0d034f47b5d2ba8f23015535006fc352aa588cdd
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-02-06 09:22:06 +0900

    Initial commit

---
+++ lib/diceware.ex
@@ -0,0 +1,18 @@
+defmodule Diceware do
+  @moduledoc """
+  Documentation for `Diceware`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Diceware.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
